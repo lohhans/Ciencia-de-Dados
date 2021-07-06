6af26d4f2cebefcf4e11c0859d4c41cf9e7b78ad
Armstrong Lohãns <lohansdemelo1108@gmail.com>
2021-07-06 19:48:02 -0300

    setup

--- 2VA/setup.sh
+++ 2VA/setup.sh
@@ -5,10 +5,4 @@ echo "\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-[theme]\n\
-primaryColor = '#933252'\n\
-backgroundColor = '#0e1117'\n\
-secondaryBackgroundColor = '#31333f'\n\
-textColor = '#FFFFFF'\n\
-font = ‘sans serif’\n\
 " > ~/.streamlit/config.toml
